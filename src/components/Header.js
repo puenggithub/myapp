5c67796181dae3600aaab595c0dbdfd4e8d61dfc
paphatsiri <paphatsiri.work@gmail.com>
2019-08-22 00:43:04 +0700

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React from 'react';
 
 const Form = () => {
   return (
-    <div className="section is-fullheight">
-      <div className="container">
-        <div className="column is-4 is-offset-4">
-          <div className="box">
-           
-          </div>
-        </div>
-      </div>
+    <div>
+      <p>test</p>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="#">&#9776; React-Bootstrap
+          <img src={logo} style={{width:100, marginTop: -7}} />
+          </a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
     </div>
+   
   );
 };
 

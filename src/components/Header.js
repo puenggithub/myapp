587deb1d47da6de15eb451d61474a36f02ab8584
paphatsiri <paphatsiri.work@gmail.com>
2019-08-22 00:43:36 +0700

    up

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,7 @@ const Form = () => {
   return (
     <div>
       <p>test</p>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#">&#9776; React-Bootstrap
-          <img src={logo} style={{width:100, marginTop: -7}} />
-          </a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
+      
     </div>
    
   );
